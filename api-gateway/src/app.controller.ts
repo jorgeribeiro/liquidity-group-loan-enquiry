1cac1bef0b6de93b71051476741c73469cfc9608
Jorge Ribeiro <joorgemelo@gmail.com>
2023-05-22 08:45:59 +0400

    Revert "Update parameter type"
    This reverts commit 5d14021fe02a16df23ed03b32b682a12ac3ee72f.

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -25,7 +25,7 @@ export class AppController {
 
   @Get()
   getLoansByYear(
-    @Param('year') year: number,
+    @Query('year') year: number,
     @Query('default') dft?: string,
     @Query('job') job?: string,
     @Query('marital') marital?: string,
